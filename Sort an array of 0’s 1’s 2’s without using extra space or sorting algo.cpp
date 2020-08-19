07d0c210d640567b351a0f61515f829e0806497d
Yashi Srivastava <yashi.18ben1017@abes.ac.in>
2020-08-19 14:30:47 +0530

    Update Sort an array of 0’s 1’s 2’s without using extra space or sorting algo.cpp

--- Sort an array of 0’s 1’s 2’s without using extra space or sorting algo.cpp
+++ Sort an array of 0’s 1’s 2’s without using extra space or sorting algo.cpp
@@ -1,4 +1,4 @@
-//My solution for time complexity o(nlogn) and space complexity o(1)
+//Brute force approach with time complexity o(nlogn) and space complexity o(1)
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
@@ -9,7 +9,34 @@ public:
     }
 };
 
-//Now , the optimized solution in time complexity o(n) and space complexity o(1) commonly known as "DUTCH NATIONAL FLAG ALGORITHM'
+//Better approach with time complexity o(2n) and space complexity o(1)
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        unordered_map<int , int> m;
+        for(auto & it: nums)
+        {
+            m[it]++;
+        }
+        
+        for(int i = 0;i<m[0];i++)
+        {
+            nums[i]=0;
+        }
+        for(int j = 0;j<m[1];j++)
+        {
+            nums[m[0]+j] = 1;
+        }
+         for(int j = 0;j<m[2];j++)
+        {
+            nums[m[1]+m[0]+j] = 2;
+        }
+        return ;
+    }
+};
+
+
+//Now , the optimized solution with time complexity o(n) and space complexity o(1) in only one pass commonly known as "DUTCH NATIONAL FLAG ALGORITHM'
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
