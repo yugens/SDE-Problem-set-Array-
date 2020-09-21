f47bc2c344f1140bfcec293b56a6625d3e7e9d2f
Yashi Srivastava <yashi.18ben1017@abes.ac.in>
2020-09-21 23:57:19 +0530

    Create Rotate Matrix.cpp

---
+++ Rotate Matrix.cpp
@@ -0,0 +1,18 @@
+//Optimised Solution
+//Time complexity : o(n^2) , Space Complexity : o(1)
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<i;j++)
+                swap(matrix[i][j],matrix[j][i]);
+        }
+        for(int i=0;i<n;i++)
+        reverse(matrix[i].begin(),matrix[i].end());
+        return;
+        
+    }
+};
