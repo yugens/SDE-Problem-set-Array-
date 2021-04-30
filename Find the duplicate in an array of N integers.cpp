0c813aeb466475fd3512a14a046f58d26b5b4a10
Yashi Srivastava <yashi.18ben1017@abes.ac.in>
2021-04-30 23:40:06 +0530

    Update Find the duplicate in an array of N integers.cpp

--- Find the duplicate in an array of N integers.cpp
+++ Find the duplicate in an array of N integers.cpp
@@ -15,7 +15,27 @@ public:
     }
 };
 
-
+//SLow and fast pointer approach with T.C. o(n) and S.C. o(1)
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+       int slw=nums[0];
+       int fst=nums[0];
+       do
+       {
+         slw = nums[slw];
+         fst=nums[nums[fst]];
+       }
+        while(slw!=fst);
+        fst=nums[0];
+        while(slw!=fst)
+        {
+            slw=nums[slw];
+            fst=nums[fst];
+        }
+        return slw;
+    }
+};
 
 
 
